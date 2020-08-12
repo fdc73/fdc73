0eed4b2e1e8e92471c72572aae297454043bf0b8
fdc73 <69454701+fdc73@users.noreply.github.com>
2020-08-12 10:30:26 +0200

    Add files via upload

---
+++ data/postedetirsurlev_3.js
@@ -0,0 +1,29 @@
+var json_postedetirsurlev_3 = {
+"type": "FeatureCollection",
+"name": "postedetirsurlev_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "ogc_fid": "69", "fid": "118", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.512988724948961, 45.477230033561767 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "79", "fid": "129", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.512159663423843, 45.458646606452334 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "80", "fid": "130", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.513809100772815, 45.466916574883491 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "81", "fid": "131", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.515386022404758, 45.462401504492533 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "82", "fid": "132", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.516535081975581, 45.471312026022225 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "83", "fid": "133", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.517776193023082, 45.469703265158401 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "84", "fid": "134", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.518638701049838, 45.46588115879775 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "85", "fid": "135", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.506038495138458, 45.459798355107125 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "86", "fid": "136", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.498223294381138, 45.464660457473585 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "88", "fid": "138", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.501774572515972, 45.460525994235027 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "89", "fid": "139", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.502378872662091, 45.465643341803172 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "90", "fid": "140", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.504525305492368, 45.463274889197045 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "91", "fid": "141", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.506701391331314, 45.453833622624977 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "92", "fid": "142", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.508814724888057, 45.473916196750906 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "93", "fid": "143", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.509698888420187, 45.476936752953748 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "94", "fid": "144", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.510854845842147, 45.461644238224771 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "103", "fid": "153", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.508049482423083, 45.480661454924977 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "104", "fid": "154", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.50046561531626, 45.46896465397969 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "105", "fid": "155", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.501463848610074, 45.474145921589191 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "106", "fid": "156", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.499060768539589, 45.450109684663076 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "107", "fid": "157", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.502215603639058, 45.454126242583101 ] } },
+{ "type": "Feature", "properties": { "ogc_fid": "108", "fid": "158", "type": "Poste de tir surélevé", "identifiant": "730361" }, "geometry": { "type": "Point", "coordinates": [ 6.502651806509638, 45.45572054140429 ] } }
+]
+}
